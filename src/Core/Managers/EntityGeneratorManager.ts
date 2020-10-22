37c949e797669d4cad77531dd5b9c55768c31d0a
Frederik Rossouw <FRossouw@SINGULAR.CO.ZA>
2020-10-22 16:49:03 +0200

    command fix

--- src/Core/Managers/EntityGeneratorManager.ts
+++ src/Core/Managers/EntityGeneratorManager.ts
@@ -1,6 +1,4 @@
 import * as vscode from "vscode";
-import WebApi from "./../Xrm/WebApi";
-import IEntity from "./../../Entities/IEntity";
 
 export default class EntityGeneratorManager {
     private context: vscode.ExtensionContext;
@@ -10,13 +8,15 @@ export default class EntityGeneratorManager {
     }
 
     public registerCommands(): void {
-        this.context.subscriptions.push(vscode.commands.registerCommand('cha0s2nd-vscode-cds.entities.generate', async () => { this.generate("k3c_contracts"); }));
+        this.context.subscriptions.push(vscode.commands.registerCommand('cha0s2nd-vscode-cds.entity.generateJs', async () => { this.generateJavascript("k3c_contract"); }));
+        this.context.subscriptions.push(vscode.commands.registerCommand('cha0s2nd-vscode-cds.entity.generateTs', async () => { this.generateTypescript("k3c_contract"); }));
     }
 
-    private async generate(entitySet: string) {
+    private async generateJavascript(entityLogicalName: string) {
+        console.log("Hi daar slaai blaar");
+    }
 
-
-console.log("Hi daar slaai blaar");
-
+    private async generateTypescript(entityLogicalName: string) {
+        console.log("Hi daar slaai blaar");
     }
 }
